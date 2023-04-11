4a241a07efe443dd1370fbc01ae451a2b27568ed
gimi65536 <gimi65536@hotmail.com>
2023-04-11 22:17:59 +0900

    Clear default styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import { Open_Sans, Noto_Sans_JP, Noto_Sans_TC } from 'next/font/google'
+
+const open_sans = Open_Sans({ subsets: ['latin', 'latin-ext'] })
+const noto_sans_jp = Noto_Sans_JP({ weight: '400', preload: false })
+const noto_sans_tc = Noto_Sans_TC({ weight: '400', preload: false })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style global jsx>{`
+        html {
+          font-family: ${open_sans.className} ${noto_sans_jp.className} ${noto_sans_tc.className};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }

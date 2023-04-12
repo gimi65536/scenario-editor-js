dcc28475bcb7624ef90cb4f1455417077d94f2ff
gimi65536 <gimi65536@hotmail.com>
2023-04-12 23:08:16 +0900

    Basic JSON schema validate The standard for "gimi65536" scenario was clear only in program layer, so I write a schema for  "gimi65536 0.0.3" (but more strict to the Python editor in `ntuccc/ScenarioEditor`). Although the definition does not match what I thought before as it restricts the required properties in scenario info, etc, my format is never used by other people's projects... So, no problem.

---
+++ pages/test-scenario.js
@@ -0,0 +1,40 @@
+import {Button} from '@mui/material';
+import {useState} from 'react';
+import { processObject2Scenario } from '@/lib/scenario';
+
+export default function TestScenario(){
+	const [scenario, setScenario] = useState(null);
+
+	function handleFileChange(e){
+		const file = e.target.files[0];
+		e.target.value = '';
+		if (file === undefined) {
+			return;
+		}
+		console.log(file);
+		const reader = new FileReader();
+		reader.onload = async (e) => {
+			const json = JSON.parse(e.target.result);
+			// Do sanitization here...
+			console.log(json);
+			setScenario(json);
+			await processObject2Scenario(json);
+		};
+		reader.readAsText(file);
+	}
+
+	return (
+		<>
+			<Button variant="contained" component="label">
+				Upload
+				<input
+					hidden
+					accept="application/json"
+					type="file"
+					onChange={handleFileChange}
+				/>
+			</Button>
+			<textarea defaultValue={scenario && JSON.stringify(scenario)} />
+		</>
+	);
+}
